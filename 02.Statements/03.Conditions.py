21bb6c25d46d66726df1197b32775ce17d849512
mujahed85 <mujahed.mca@gmail.com>
2022-05-10 05:16:48 +0530

    Conditional Statements explained

---
+++ 02.Statements/03.Conditions.py
@@ -0,0 +1,7 @@
+strAge=input("Enter Your Age: ")
+iAge=int(strAge)
+
+if iAge>=18 and iAge<=30:
+    print("Vote")
+else:
+    print("No Vote")
---
+++ 02.Statements/04.Conditions2.py
@@ -0,0 +1,10 @@
+name=input("Enter Name: ")
+
+if name=="macduff":
+    print("Macduff is Software Engineer")
+    print("Macduff is Data Engineer")
+    print("Macduff is Cloud Engineer")
+if name=="akwasi":
+    print("akwasi is Software Principal Engineer")
+    print("akwasi is BigData Engineer")
+    print("akwasi is Cloud Engineer")
